ae96269591d157df706cc281ccd611dc45f0db04
Devanshu Sharma <er.devanshusharma@gmail.com>
2013-03-27 16:31:58 -0700

    Adding first draft for chef

---
+++ .chef/knife.rb
@@ -0,0 +1,9 @@
+log_level                :info
+log_location             STDOUT
+node_name                'devanshusharma'
+client_key               '/Users/devanshusharma/chef-code/.chef/devanshusharma.pem'
+validation_client_name   'chef-validator'
+validation_key           '/etc/chef/chef-validator.pem'
+chef_server_url          'https://17.209.114.180'
+cache_type               'BasicFile'
+cache_options( :path => '/Users/devanshusharma/chef-code/.chef/checksums' )
